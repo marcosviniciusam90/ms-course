b20a15c2d8ad45ffe9e8aae273381e9dab1c3ebe
Marcos <marcos.mendonca@matera.com>
2021-05-30 03:17:45 -0300

    Ribbon load balancing

--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -2,7 +2,10 @@ package com.devsuperior.hrworker.resources;
 
 import com.devsuperior.hrworker.entities.Worker;
 import com.devsuperior.hrworker.repositories.WorkerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +17,11 @@ import java.util.List;
 @RequestMapping("/workers")
 public class WorkerResource {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(WorkerResource.class);
+
+    @Autowired
+    private Environment env;
+
     @Autowired
     private WorkerRepository workerRepository;
 
@@ -24,6 +32,8 @@ public class WorkerResource {
 
     @GetMapping("/{id}")
     public Worker findById(@PathVariable Long id) {
+
+        LOGGER.info("PORT = " + env.getProperty("local.server.port"));
         return workerRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("Resource not found"));
     }

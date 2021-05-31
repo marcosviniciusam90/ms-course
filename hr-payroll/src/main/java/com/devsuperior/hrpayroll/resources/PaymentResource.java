16050c61faf4da726cde3cb83a632b1549906f24
Marcos <marcos.mendonca@matera.com>
2021-05-31 05:54:44 -0300

    Hystrix fault tolerance

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/resources/PaymentResource.java
@@ -2,6 +2,7 @@ package com.devsuperior.hrpayroll.resources;
 
 import com.devsuperior.hrpayroll.entities.Payment;
 import com.devsuperior.hrpayroll.services.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,8 +16,13 @@ public class PaymentResource {
     @Autowired
     private PaymentService paymentService;
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative" )
     @GetMapping("/{workerId}/days/{days}")
     public Payment getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
         return paymentService.getPayment(workerId, days);
+    }
+
+    public Payment getPaymentAlternative(Long workerId, Integer days) {
+        return new Payment("Marcos", 400.0, days);
     }
 }
--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -33,6 +33,14 @@ public class WorkerResource {
     @GetMapping("/{id}")
     public Worker findById(@PathVariable Long id) {
 
+        /*
+        try {
+            Thread.sleep(3000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        */
+
         LOGGER.info("PORT = " + env.getProperty("local.server.port"));
         return workerRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("Resource not found"));

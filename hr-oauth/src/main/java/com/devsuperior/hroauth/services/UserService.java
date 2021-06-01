d094db49b2a40ef97133082dc444516e9f450a3e
Marcos <marcos.mendonca@matera.com>
2021-06-01 20:57:08 -0300

    OAuth2 Login + JWT Token

--- hr-oauth/src/main/java/com/devsuperior/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/devsuperior/hroauth/services/UserService.java
@@ -3,15 +3,29 @@ package com.devsuperior.hroauth.services;
 import com.devsuperior.hroauth.entities.User;
 import com.devsuperior.hroauth.feignclients.UserFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     @Autowired
     private UserFeignClient userFeignClient;
 
     public User findByEmail(String email) {
         return userFeignClient.findByEmail(email);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = null;
+        try {
+            user = findByEmail(username);
+        } catch (RuntimeException ex) {
+            throw new UsernameNotFoundException("Username not found");
+        }
+        return user;
     }
 }

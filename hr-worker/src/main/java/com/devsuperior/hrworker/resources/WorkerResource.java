d11f694ad3dc3bd06c8a8337869051cb21dc7e99
Marcos <marcos.mendonca@matera.com>
2021-06-02 21:25:59 -0300

    H2 Database properties in Config Server

--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -5,8 +5,6 @@ import com.devsuperior.hrworker.repositories.WorkerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,26 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RefreshScope
 @RestController
 @RequestMapping("/workers")
 public class WorkerResource {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(WorkerResource.class);
 
-    @Value("${test.config}")
-    private String testConfig;
-
     @Autowired
     private Environment env;
 
     @Autowired
     private WorkerRepository workerRepository;
-
-    @GetMapping("/configs")
-    public String getConfigs() {
-        return "CONFIG = " + testConfig;
-    }
 
     @GetMapping
     public List<Worker> findAll() {
@@ -43,15 +32,6 @@ public class WorkerResource {
 
     @GetMapping("/{id}")
     public Worker findById(@PathVariable Long id) {
-
-        /*
-        try {
-            Thread.sleep(3000L);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        */
-
         LOGGER.info("PORT = " + env.getProperty("local.server.port"));
         return workerRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("Resource not found"));
